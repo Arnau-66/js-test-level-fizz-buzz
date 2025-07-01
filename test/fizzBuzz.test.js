f948e5da71e65e77d8dd78788040e81561b5d5b4
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-01 12:27:56 +0200

    Testing 'returns number when is not multiple of 3 and 5'

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -94,6 +94,24 @@ describe('FizzBuzz test for multiples of 3 and 5', () => {
              * When el número es procesado
              * Then se muestra el número ingresado
              */
+
+            // Arrange
+            let valor_entrada = 8;
+            let respuesta_esperada = {
+                status: "ok", // Código indicando éxito
+                message: "El número no es divisible por 3 ni por 5", // Mensaje de validación
+                data: {
+                    input: 8,  // Número evaluado
+                    output: valor_entrada // Resultado esperado
+                }}
+            
+            // Act
+            let resultado = fizzBuzz(valor_entrada);
+
+            // Assert
+            expect(typeof resultado.data.input).toBe("number"); // valida que input sea un número
+            expect(resultado).toEqual(respuesta_esperada); // valida estructura de respuesta
+            expect(resultado.data.output).toBe(valor_entrada); // valida que el valor output corresponda al expect
         })
     }
 )
